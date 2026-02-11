159169b83d0cdf1ccdd14e73afa0e6428c9d1720
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-02-11 19:06:41 +0530

    logout correction on user dashboard

--- src/components/User/UserDashboardLayout.jsx
+++ src/components/User/UserDashboardLayout.jsx
@@ -2,9 +2,9 @@
 import { Outlet, Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FiBell, FiSearch, FiUser, FiLogOut } from "react-icons/fi";
-import { logout } from "../Utils/Logout";
-
+import { useAuth } from "../../context/AuthContext";
 export const UserDashboardLayout = () => {
+  const {logout} = useAuth();
   const location = useLocation();
 
   const navTabs = [
