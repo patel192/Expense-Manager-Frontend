c4eb5dd2b5bca048a5f24ab0543d3945c9f412fb
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-03-19 11:37:04 +0530

    fix:import statement fixed for theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { ThemeProvider } from "./context/ThemeContext.js";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
