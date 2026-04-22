395392e2f3e678acd51bf31b624b7e41df1c2bdf
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-04-22 14:06:00 +0530

    feat: import index.css for global styling in main component

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import "./index.css"
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
