c7540582039d1992b1e44e6faeeb90c00d1e7c97
Muhammad <patelmuhammad192@gmail.com>
2026-06-01 14:04:03 +0530

    fix: correct syntax for axios instance creation

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 // Base API URL from environment variable
 const instance = axios.create({
   baseURL: "https://learn-25-node-1.onrender.com/api",
-});
+})
 
 // Attach token automatically
 instance.interceptors.request.use((config) => {
